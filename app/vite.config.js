7205487bf869d25158953cfc76e684efd0266f7f
AriCourcy <aricourcy0323@gmail.com>
2026-09-18 15:12:46 -0400

    added sinatra static render and poc proxy

--- app/vite.config.js
+++ app/vite.config.js
@@ -5,6 +5,11 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   base: './',
 
+  build: {
+    outDir: "../public",
+    emptyOutDir: true
+  },
+
   plugins: [
     react(),
 
---
+++ server.rb
@@ -0,0 +1,59 @@
+require "bundler/inline"
+
+gemfile do
+    source "http://rubygems.org"
+
+    gem "sinatra-contrib"
+
+    gem "rackup"
+    gem "puma"
+    gem "rest-client"
+end
+
+require "sinatra/base"
+require "sinatra/reloader"
+
+class MySinatraApp < Sinatra::Base
+    set :public_folder, File.expand_path("public", __dir__)
+    set :static, true
+
+    configure :development do
+        register Sinatra::Reloader
+    end
+
+    get "/api/googlebooks" do
+      query = params["query"]
+      key = params["key"]
+
+      response = RestClient.get(
+        "https://www.googleapis.com/books/v1/volumes", {
+          params: {
+            "q"=> query,
+            "maxResults" => 10,
+            "key" => key,
+            "fields" => "items(id,volumeInfo(title,authors,industryIdentifiers,imageLinks,pageCount,publishedDate,publisher,previewLink,infoLink))"
+          }
+      })
+
+    end 
+
+    get "/api/openlibrary" do
+      query = params["query"]
+
+      response = RestClient.get(
+        "https://openlibrary.org/search.json", {
+          params: {
+            "q"=> query,
+            "limit" => 10,
+            "fields" => "title,author_name,isbn,cover_i,number_of_pages_median,first_publish_year,publisher,key,edition_key,first_sentence"
+          }
+      })
+
+    end
+
+    get "*" do
+      send_file File.join(settings.public_folder, "index.html")
+    end
+
+    run! if app_file == $0
+end
